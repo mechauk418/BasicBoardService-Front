52e355b468b5925bf849696a75ed171f21d70c3c
mechauk418 <mechauk418@gmail.com>
2023-04-25 15:41:58 +0900

    img fix2

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -5,9 +5,9 @@
       <div class="title"><p>{{article_title  }}</p></div>
       <div class="writer"><p> 작성자 :  {{ article_user }}</p> </div>
       <div class="content"> <p> 내용 : {{ article_content }}</p></div>
-      <div v-for="image in article_image" :key="image" style="position: absolute; width:60%;">
-        <img :src="image.image_original" v-if="image.image == null">
-        <img :src="image.image" v-else>
+      <div v-for="image in article_image" :key="image">
+        <img :src="image.image_original" v-if="image.image == null"  style="position: absolute; width:60%;">
+        <img :src="image.image" v-else  style="position: absolute; width:60%;">
       </div>
       <p> 추천수 : {{ article_like }}</p>
       <div class="div_btn">
